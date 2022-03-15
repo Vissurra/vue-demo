0f7e2d95e368d7780aa323b24d5581fe31e89181
林俊 <461968025@qq.com>
2022-03-15 15:52:40 +0800

    使用自动导入

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ]
+  }
 })
