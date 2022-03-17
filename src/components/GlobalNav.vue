c65dcae98263f075e53cd5b89de932a16ece8953
林俊 <461968025@qq.com>
2022-03-17 13:27:36 +0800

    增加导航条可点击区域

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -4,22 +4,22 @@
       mode="horizontal"
       @select="handleSelect"
   >
-    <el-menu-item index="1">
-      <router-link to="/">Home</router-link>
-    </el-menu-item>
+    <router-link to="/">
+      <el-menu-item index="1">Home</el-menu-item>
+    </router-link>
     <el-sub-menu index="2">
       <template #title>Users</template>
-      <el-menu-item index="2-1">
-        <router-link to="/user/1">User1</router-link>
-      </el-menu-item>
-      <el-menu-item index="2-2">
-        <router-link to="/user/2">User2</router-link>
-      </el-menu-item>
+      <router-link to="/user/1">
+        <el-menu-item index="2-1">User1</el-menu-item>
+      </router-link>
+      <router-link to="/user/2">
+        <el-menu-item index="2-2">User2</el-menu-item>
+      </router-link>
     </el-sub-menu>
     <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">
-      <router-link to="/about">About</router-link>
-    </el-menu-item>
+    <router-link to="/about">
+      <el-menu-item index="4">About</el-menu-item>
+    </router-link>
   </el-menu>
 </template>
 
