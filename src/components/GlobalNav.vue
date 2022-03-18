c9965ed3f39a06eed24ec5e4d7c93185c2e0889c
Vissurra <vissurra@gmail.com>
2022-03-18 09:56:59 +0800

    nav启用route模式

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,37 +1,24 @@
 <template>
   <el-menu
-      :default-active="activeIndex"
+      :default-active="$route.path"
       mode="horizontal"
       @select="handleSelect"
+      router
   >
-    <router-link to="/">
-      <el-menu-item index="1">Home</el-menu-item>
-    </router-link>
+    <el-menu-item index="/">Home</el-menu-item>
     <el-sub-menu index="2">
       <template #title>Users</template>
-      <router-link to="/user/1">
-        <el-menu-item index="2-1">User1</el-menu-item>
-      </router-link>
-      <router-link to="/user/2">
-        <el-menu-item index="2-2">User2</el-menu-item>
-      </router-link>
+      <el-menu-item index="/user/1">User1</el-menu-item>
+      <el-menu-item index="/user/2">User2</el-menu-item>
     </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <router-link to="/about">
-      <el-menu-item index="4">About</el-menu-item>
-    </router-link>
+    <el-menu-item index="/info" disabled>Info</el-menu-item>
+    <el-menu-item index="/about">About</el-menu-item>
   </el-menu>
 </template>
 
 <script>
 export default {
   name: 'GlobalNav',
-  data() {
-    return {
-      url: '../assets/logo.png',
-      activeIndex: "1"
-    }
-  },
   methods: {
     handleSelect: (key, keyPath) => {
       console.log(key, keyPath)
--- src/components/UserNav.vue
+++ src/components/UserNav.vue
@@ -1,23 +1,19 @@
 <template>
   <el-row>
     <el-col :span="14" :offset="4">
-      <el-menu default-active="1">
-        <router-link :to="'/user/' + id + '/profile'">
-          <el-menu-item index="1">
-            <el-icon>
-              <avatar/>
-            </el-icon>
-            Profile
-          </el-menu-item>
-        </router-link>
-        <router-link :to="'/user/' + id + '/posts'">
-          <el-menu-item index="2">
-            <el-icon>
-              <list/>
-            </el-icon>
-            Posts
-          </el-menu-item>
-        </router-link>
+      <el-menu default-active="/" router>
+        <el-menu-item :index="'/user/' + id + '/profile'">
+          <el-icon>
+            <avatar/>
+          </el-icon>
+          Profile
+        </el-menu-item>
+        <el-menu-item :index="'/user/' + id + '/posts'">
+          <el-icon>
+            <list/>
+          </el-icon>
+          Posts
+        </el-menu-item>
       </el-menu>
     </el-col>
   </el-row>
